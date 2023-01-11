d9bf048dee0589aa105713c732eebc293f37aa4c
jonathanpoon1 <115601929+jonathanpoon1@users.noreply.github.com>
2023-01-11 15:51:40 -0500

    Add files via upload

--- fullpage/fullpage.js
+++ fullpage/fullpage.js
@@ -1111,6 +1111,11 @@
       scrollBar: false,
       hybrid: false,
       licenseKey: '',
+      credits: {
+        "enabled": true,
+        "label": 'Made with fullPage.js',
+        "position": 'right'
+      },
       //scrolling
       css3: true,
       scrollingSpeed: 700,
